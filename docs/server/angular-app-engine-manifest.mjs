336ae55ded098e3a17a702f6109708914a1847bc
Lokenzen <tom@kernoa.com>
2025-06-25 15:57:25 +0200

    rebuild

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/delkia',
+  basePath: '/Delkia',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/delkia',
+  baseHref: '/Delkia',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/delkia"
+    "route": "/Delkia"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23719, hash: '59433a0c5a90ed8420b6baefec392f5c37f9e57add2b46f1371c6462ba674392', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17293, hash: 'b90b250b2ea25e57dcd839f517fc2fa4319bf89332bc79948badb1346ebc6e7e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 82482, hash: 'c1e5f1b6425c99b59ef6996aea3dfc2371b644e32f1af6ca94fa31abc05654bc', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23719, hash: '47d8bebb66f6da01fb7efa07432c16f3a0964183ecf61b55e32de736887f08a8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17293, hash: '6d3d97be185e8593d2fd38ffb4f3bfcee10a35d0a97ea24945ea4ead10c0a190', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 82482, hash: 'b9525f59f37deb5f733b23e7259a0135f7092ce93b3346660275253a5a429d00', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-52WF6A3K.css': {size: 6979, hash: 'ob/HeOe/41A', text: () => import('./assets-chunks/styles-52WF6A3K_css.mjs').then(m => m.default)}
   },
 };
