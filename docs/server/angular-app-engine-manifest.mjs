e1edfd0ed128236b63c98ea7e0548f1828882059
Lokenzen <tom@kernoa.com>
2025-06-25 16:13:33 +0200

    redeploy

--- docs/server/angular-app-engine-manifest.mjs
+++ docs/server/angular-app-engine-manifest.mjs
@@ -1,6 +1,6 @@
 
 export default {
-  basePath: '/Delkia',
+  basePath: '/',
   supportedLocales: {
   "en-US": ""
 },
--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -2,19 +2,19 @@
 export default {
   bootstrap: () => import('./main.server.mjs').then(m => m.default),
   inlineCriticalCss: true,
-  baseHref: '/Delkia',
+  baseHref: '/',
   locale: undefined,
   routes: [
   {
     "renderMode": 2,
-    "route": "/Delkia"
+    "route": "/delkia"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23719, hash: '47d8bebb66f6da01fb7efa07432c16f3a0964183ecf61b55e32de736887f08a8', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17293, hash: '6d3d97be185e8593d2fd38ffb4f3bfcee10a35d0a97ea24945ea4ead10c0a190', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 82482, hash: 'b9525f59f37deb5f733b23e7259a0135f7092ce93b3346660275253a5a429d00', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23721, hash: 'fbaf4e29c2a3cc683be7586a676cfe615748c0709ddde6baf1cc472fcb6e81d0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17295, hash: '18657d335edda7780bb2680028f535a27c9eb8993281751e44c6346b38284960', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'delkia/index.html': {size: 82484, hash: '56d9844dac6b9dec836fc7d0b38965a853e6a2a8b2b375dc0ab5d359479401c7', text: () => import('./assets-chunks/delkia_index_html.mjs').then(m => m.default)},
     'styles-52WF6A3K.css': {size: 6979, hash: 'ob/HeOe/41A', text: () => import('./assets-chunks/styles-52WF6A3K_css.mjs').then(m => m.default)}
   },
 };
