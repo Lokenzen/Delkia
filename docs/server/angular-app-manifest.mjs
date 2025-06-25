044d82b0ef55493614e558aab3824992729d5f72
Lokenzen <tom@kernoa.com>
2025-06-25 15:09:34 +0200

    new deploy

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -12,9 +12,9 @@ export default {
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23719, hash: 'a965537cfda0709eb8ec244f8c913a810e65189d418061b635480aeff8f40b1b', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17293, hash: 'e7fb3450d3e15034aa1b348c40adb2281c3f2f80fc33a9404e20f729ab4c1dab', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'index.html': {size: 82165, hash: '70624d18b90fa7fc06147e427be3514900a3a1c32ec6e46bc0c895bba5a134bd', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23719, hash: '59433a0c5a90ed8420b6baefec392f5c37f9e57add2b46f1371c6462ba674392', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17293, hash: 'b90b250b2ea25e57dcd839f517fc2fa4319bf89332bc79948badb1346ebc6e7e', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 82482, hash: 'c1e5f1b6425c99b59ef6996aea3dfc2371b644e32f1af6ca94fa31abc05654bc', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-52WF6A3K.css': {size: 6979, hash: 'ob/HeOe/41A', text: () => import('./assets-chunks/styles-52WF6A3K_css.mjs').then(m => m.default)}
   },
 };
