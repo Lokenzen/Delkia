8a6652e582ab35d7b314b85819fd59515a0d1007
Lokenzen <tom@kernoa.com>
2025-06-25 16:17:45 +0200

    change href

--- docs/server/angular-app-manifest.mjs
+++ docs/server/angular-app-manifest.mjs
@@ -7,14 +7,14 @@ export default {
   routes: [
   {
     "renderMode": 2,
-    "route": "/delkia"
+    "route": "/"
   }
 ],
   entryPointToBrowserMapping: undefined,
   assets: {
-    'index.csr.html': {size: 23721, hash: 'fbaf4e29c2a3cc683be7586a676cfe615748c0709ddde6baf1cc472fcb6e81d0', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
-    'index.server.html': {size: 17295, hash: '18657d335edda7780bb2680028f535a27c9eb8993281751e44c6346b38284960', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
-    'delkia/index.html': {size: 82484, hash: '56d9844dac6b9dec836fc7d0b38965a853e6a2a8b2b375dc0ab5d359479401c7', text: () => import('./assets-chunks/delkia_index_html.mjs').then(m => m.default)},
+    'index.csr.html': {size: 23714, hash: 'da464310955f94c94f8fd386efac45739f86a5204540b4a1d482828dbd291919', text: () => import('./assets-chunks/index_csr_html.mjs').then(m => m.default)},
+    'index.server.html': {size: 17288, hash: '62c49e50ca14309fe36c987bd971c250372d818d296ecb8c8704438c56f4cb9b', text: () => import('./assets-chunks/index_server_html.mjs').then(m => m.default)},
+    'index.html': {size: 82477, hash: 'ce229ded557d09bc428f61d1b43dff2bbadf882c16cb8510c80c0eabf1804e46', text: () => import('./assets-chunks/index_html.mjs').then(m => m.default)},
     'styles-52WF6A3K.css': {size: 6979, hash: 'ob/HeOe/41A', text: () => import('./assets-chunks/styles-52WF6A3K_css.mjs').then(m => m.default)}
   },
 };
